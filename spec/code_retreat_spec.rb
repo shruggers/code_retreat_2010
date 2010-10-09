e84dccb5799a3c6e3431edd705ccc4142951110d
James Almond <james@jamesalmond.com>
2010-10-09 14:38:19 +0100

    Red

--- spec/code_retreat_spec.rb
+++ spec/code_retreat_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 require 'code_retreat'
 
-describe "code_retreat" do
-  it "does something" do
-    fail "needs some specs"
+describe GameOfLife do
+  
+  context "you have one active cell" do
+    it "should get no cell back" do
+      game = GameOfLife.new(Cell.new(0,0))
+      game.tick.should == []
+    end
   end
+  
 end
