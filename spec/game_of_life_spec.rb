ced37d978d5729597a4ba3020be73e65356f501f
James Almond <james@jamesalmond.com>
2010-10-09 10:17:36 +0100

    Green

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -4,11 +4,21 @@ require 'game_of_life'
 describe GameOfLife do
   
   context "a single cell" do
-    let(:grid){[[true]]}
-    it "should kill the cell" do
-      game = GameOfLife.new(grid)
-      game.tick
-      game.grid.should == [[false]]
+    context "where the cell is dead" do
+      let(:grid){[[false]]}
+      it "should kill the cell" do
+        game = GameOfLife.new(grid)
+        game.tick
+        game.grid.should == [[false]]
+      end
+    end
+    context "where the cell is alive" do
+      let(:grid){[[true]]}
+      it "should kill the cell" do
+        game = GameOfLife.new(grid)
+        game.tick
+        game.grid.should == [[false]]
+      end
     end
   end
   
