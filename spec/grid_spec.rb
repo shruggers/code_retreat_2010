fc112e5a14449ade6b909d67f811a2447af8502c
James Almond <james@jamesalmond.com>
2010-10-09 11:56:55 +0100

    Red

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -4,6 +4,11 @@ require 'code_retreat'
 describe Grid do
   
   describe "co-ordinates" do
+    let(:cells){
+      [[true, false, true],
+      [true, false, true],
+      [false, false, false]]
+    }
     
   end
   
