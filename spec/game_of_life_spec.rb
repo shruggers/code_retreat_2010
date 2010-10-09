6dd1d26fdd28630067a126917df24d4524965d5d
James Almond <james@jamesalmond.com>
2010-10-09 10:11:31 +0100

    Red

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -3,4 +3,13 @@ require 'code_retreat'
 
 describe GameOfLife do
   
+  context "a single cell" do
+    let(:grid){[[true]]}
+    it "should kill the cell" do
+      game = GameOfLife.new(grid)
+      game.tick
+      game.grid.should == [[false]]
+    end
+  end
+  
 end
