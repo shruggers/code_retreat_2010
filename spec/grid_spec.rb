0caead30f4163c8ee950f0d7aa330694d287ac91
James Almond <james@jamesalmond.com>
2010-10-09 11:59:21 +0100

    Red

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -10,6 +10,15 @@ describe Grid do
       [false, false, false]]
     }
     
+    (0..2).each do |rows|
+      (0..2).each do |columns|
+        it "should have a cell for (#{row}, #{column}) with the right value" do
+          grid = Grid.new(cells)
+          grid.get(row, column).alive?.should == cells[row][column]
+        end
+      end
+    end
+    
   end
   
 end
