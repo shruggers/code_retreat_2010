893662c5d48ef4b283e5bae931118d468f3609fe
James Almond <james@jamesalmond.com>
2010-10-09 11:52:27 +0100

    Red

---
+++ spec/cell_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require 'code_retreat'
+
+describe Cell do
+  
+  describe "alive?" do
+    context "where passed true" do
+      it "should be alive" do
+        Cell.new(true).should be_alive
+      end
+    end
+    context "where passed false" do
+      it "should not be alive" do
+        Cell.new(false).should_not be_alive
+      end
+    end
+  end
+  
+end
