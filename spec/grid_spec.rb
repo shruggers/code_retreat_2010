5aa353f5abaa0984ccc0e00170625ccb9d41145a
James Almond <james@jamesalmond.com>
2010-10-09 11:11:07 +0100

    Green

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,27 +2,34 @@ require 'spec_helper'
 require 'code_retreat'
 
 describe Grid do
-  
-  let(:cells){
-    [
-      [Cell.new(false),Cell.new(false),Cell.new(false)],
-      [Cell.new(false),Cell.new(false),Cell.new(false)],
-      [Cell.new(false),Cell.new(false),Cell.new(false)]
-    ]
-  }
-  
+
   describe "neighbours" do
-    let(:cells){
-      [
-        [Cell.new(false),Cell.new(false),Cell.new(false)],
-        [Cell.new(false),Cell.new(false),Cell.new(false)],
-        [Cell.new(false),Cell.new(false),Cell.new(false)]
-      ]
-    }
-    it "should have no live neighbours for the centre cell" do
-      grid = Grid.new(cells)
-      grid.live_neighbours_for(1,1).should == 0
+    context "no alive neighbours" do
+      let(:cells){
+        [
+          [Cell.new(false),Cell.new(false),Cell.new(false)],
+          [Cell.new(false),Cell.new(false),Cell.new(false)],
+          [Cell.new(false),Cell.new(false),Cell.new(false)]
+        ]
+      }
+      it "should have no live neighbours for the centre cell" do
+        grid = Grid.new(cells)
+        grid.live_neighbours_for(1,1).should == 0
+      end
     end
+    context "all live neighbours" do
+      let(:cells){
+        [
+          [Cell.new(true),Cell.new(true),Cell.new(true)],
+          [Cell.new(true),Cell.new(false),Cell.new(true)],
+          [Cell.new(true),Cell.new(true),Cell.new(true)]
+        ]
+      }
+      it "should have no live neighbours for the centre cell" do
+        grid = Grid.new(cells)
+        grid.live_neighbours_for(1,1).should == 0
+      end
+    end    
   end
   
   
