74faa2e5052a855e0567e442acde1d3875859a1d
James Almond <james@jamesalmond.com>
2010-10-09 11:08:29 +0100

    All good

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -9,7 +9,22 @@ describe Grid do
       [Cell.new(false),Cell.new(false),Cell.new(false)],
       [Cell.new(false),Cell.new(false),Cell.new(false)]
     ]
-    
   }
+  
+  describe "neighbours" do
+    let(:cells){
+      [
+        [Cell.new(false),Cell.new(false),Cell.new(false)],
+        [Cell.new(false),Cell.new(false),Cell.new(false)],
+        [Cell.new(false),Cell.new(false),Cell.new(false)]
+      ]
+    }
+    it "should have no live neighbours for the centre cell" do
+      grid = Grid.new(cells)
+      grid.live_neighbours_for(1,1).should == 0
+    end
+  end
+  
+  
   
 end
