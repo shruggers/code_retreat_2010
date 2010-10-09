da2100f58296a57c9996b9bc11d6f8b5c51c31cd
James Almond <james@jamesalmond.com>
2010-10-09 12:10:21 +0100

    Red

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -6,17 +6,17 @@ describe Cell do
   describe "alive?" do
     context "where passed true" do
       it "should be alive" do
-        Cell.new(true).should be_alive
+        Cell.new(1,true).should be_alive
       end
     end
     context "where passed false" do
       it "should not be alive" do
-        Cell.new(false).should_not be_alive
+        Cell.new(1,false).should_not be_alive
       end
     end
     context "where not passed anything" do
       it "should return false" do
-        Cell.new.should_not be_alive
+        Cell.new(1).should_not be_alive
       end
     end
   end
