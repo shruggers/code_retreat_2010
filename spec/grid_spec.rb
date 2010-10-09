ac3ef64ba5b665302260268aa05e038092ba578e
James Almond <james@jamesalmond.com>
2010-10-09 10:38:25 +0100

    Red

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -3,4 +3,16 @@ require 'game_of_life'
 require 'grid'
 
 describe Grid do
+  
+  describe "coordinates" do
+    
+    let(:grid_array){[[true, false],[false, true]]}
+    
+    it "should let me access the points at the relevant co-ordiantes" do
+      grid = Grid.new(grid_array)
+      grid.get(0,0).should be_true
+    end
+    
+  end
+  
 end
