2208670e0cfb4b53bbb39d40e6463842121bb4b8
James Almond <james@jamesalmond.com>
2010-10-09 12:06:15 +0100

    Red

--- lib/grid.rb
+++ lib/grid.rb
@@ -7,5 +7,16 @@ class Grid
     Cell.new(@cells[x][y])
   end
   
+  def alive_neighbours_for(x,y)
+    count = 0
+    (-1..1).each do |row|
+      (-1..1).each do |column|
+        unless row == 0 && column == 0
+          count += if get(x + row, y + row).alive?
+        end
+      end
+    end
+  end
+  
   
 end
